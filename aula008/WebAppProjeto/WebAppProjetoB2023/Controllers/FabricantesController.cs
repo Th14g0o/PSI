0a4f3b4a6c924679e782c032a32ddf54bf8bbfae
Th14g0o <thiago.gonsalves@escolar.ifrn.edu.br>
2023-04-23 19:02:25 -0300

    aula008 antecipada

---
+++ aula008/WebAppProjeto/WebAppProjetoB2023/Controllers/FabricantesController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppProjetoB2023.Models;
+using System.Net; // pro " new HttpStatusCodeResult(HttpStatusCode.BadRequest) " funcionar
+
+namespace WebAppProjetoB2023.Controllers
+{
+    public class FabricantesController : Controller
+    {
+        private EFContext context = new EFContext();
+        // GET: Fabricantes
+        public ActionResult Index()
+        {
+            return View(context.Fabricantes.OrderBy(c => c.nome));
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Fabricante fabricante = context.Fabricantes.Find(id);
+            if (fabricante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fabricante);
+        }
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Fabricante fabricante = context.Fabricantes.Find(id);
+            if (fabricante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fabricante);
+        }
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Fabricante fabricante = context.Fabricantes.Find(id);
+            if (fabricante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fabricante);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Fabricante fabricante)
+        {
+            context.Fabricantes.Add(fabricante);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Fabricante fabricante)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Entry(fabricante).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(fabricante);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(long id)
+        {
+            Fabricante fabricante = context.Fabricantes.Find(id);
+            context.Fabricantes.Remove(fabricante);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
