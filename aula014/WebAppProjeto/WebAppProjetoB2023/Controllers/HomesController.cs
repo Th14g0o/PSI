d38e3821bbc37a1c6bccf8c9ed8414586c3b465b
Thiago Gonsalves Da Silva <20211011110014@ifrn.local>
2023-06-20 17:56:54 -0300

    aula do dia

---
+++ aula014/WebAppProjeto/WebAppProjetoB2023/Controllers/HomesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppProjetoB2023.Models;
+
+namespace WebAppProjetoB2023.Controllers
+{
+    public class HomesController : Controller
+    {
+        EFContext context = new EFContext();
+        // GET: Homes
+        public ActionResult Index()
+        {
+            Home h = new Home();
+            h.Categorias = context.Categorias.OrderBy(c => c.Nome);
+            h.Fabricantes = context.Fabricantes.OrderBy(c => c.Nome);
+            h.Produtos = context.Produtos.OrderBy(p => p.Nome);
+            return View(h);
+        }
+        public ActionResult IndexComProdutosdoFabricante(long? id, bool tipo)
+        {
+            Home h = new Home();
+            h.Categorias = context.Categorias.OrderBy(c => c.Nome);
+            h.Fabricantes = context.Fabricantes.OrderBy(c => c.Nome);
+            if (id != null)
+            {
+                if (tipo)
+                {
+                    h.Produtos = context.Produtos.Where(p => p.FabricanteId == id);
+                }
+                else
+                {
+                    h.Produtos = context.Produtos.Where(p => p.CategoriaId == id);
+                }
+            }
+            else
+            {
+                h.Produtos = context.Produtos.OrderBy(p => p.Nome);
+            }
+            return View(h);
+        }
+    }
+}
