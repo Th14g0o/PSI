bf1d2d8beeca4b580ba75ca205aacce90c8805eb
Thiago Gonsalves Da Silva <20211011110014@ifrn.local>
2023-07-25 17:45:46 -0300

    asbhn

---
+++ aula022/WebAppProjeto/Modelo/Cadastros/Produto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Modelo.Tabelas;
+namespace Modelo.Cadastros
+{
+    public class Produto
+    {
+        public long? ProdutoId { get; set; }
+        public string Nome { get; set; }
+
+        public long? CategoriaId { get; set; } // chave estrangeira
+        public long? FabricanteId { get; set; }
+
+        public Categoria Categoria { get; set; }// para carregar o objeto com o auxilio da chave estrangeiro
+        public Fabricante Fabricante { get; set; }
+
+        public string LogotipoMimeType { get; set; } //pega o tipo(png, jpg,mp4, html,css...) - MimeType são os tipos de arquivo que o navegador pode mostrar
+        public byte[] Logotipo { get; set; }// Pega os bytes da imagem, pixeis, conteudo
+        public string NomeArquivo { get; set; }
+        public long TamanhoArquivo { get; set; }
+
+    }
+}
