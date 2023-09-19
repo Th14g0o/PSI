ae15a12db641454530276d5dec09c587377addea
Thiago Gonsalves Da Silva <20211011110014@ifrn.local>
2023-09-19 18:04:27 -0300

    cap 7 iniciado

---
+++ aula024/WebAppProjeto/Modelo/Cadastros/Fabricante.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Modelo.Tabelas;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+namespace Modelo.Cadastros
+{
+    public class Fabricante
+    {
+        [DisplayName("Codigo")]
+        public long FabricanteId { get; set; }
+        [StringLength(100, ErrorMessage = "O nome do produto precisa ter no mínimo 10 caracteres", MinimumLength = 1)]
+        [Required(ErrorMessage = "Informe o nome do Fabricante")]
+        public string Nome { get; set; }
+        public virtual ICollection<Produto> Produtos { get; set; }
+    }
+}
