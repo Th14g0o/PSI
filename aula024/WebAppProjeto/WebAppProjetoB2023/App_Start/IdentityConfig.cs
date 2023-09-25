5f84930caa846ac23ba149a2e71d7296f5bb544f
Thiago Gonsalves Da Silva <20211011110014@ifrn.local>
2023-09-25 16:04:00 -0300

    kfnbvjfnvjnbfjvjnxlvcxjlj

---
+++ aula024/WebAppProjeto/WebAppProjetoB2023/App_Start/IdentityConfig.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAppProjetoB2023.DAL;
+
+namespace WebAppProjetoB2023
+{
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext<IdentityDbContextAplicacao>(IdentityDbContextAplicacao.Create);
+            app.CreatePerOwinContext<GerenciadorUsuario>(GerenciadorUsuario.Create);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Seguranca/Account/Login"),
+            });
+        }
+    }
+}
